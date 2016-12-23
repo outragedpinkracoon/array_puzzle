72ca90ba71593dc17ead1e2b05995d53ff9bdec3
Valerie Dryden <outragedpinkracoon@gmail.com>
2016-12-23 15:02:54 +0000

    stuff

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const data = require('./data')
 Array.prototype.concatAll = function() {
   var results = [];
   this.forEach(function(subArray) {
-    // Add all the items in each subArray to the results array.
     subArray.forEach(function(subArrayItem) {
       results.push(subArrayItem)
     })
@@ -12,48 +11,26 @@ Array.prototype.concatAll = function() {
   return results;
 };
 
-const getVideos = (movieLists) => {
-	return movieLists.map(item => item.videos)
-									 .map(videos => {
-									 		return videos.map(({title, id, boxarts}) => ({
-													title,
-													id,
-													boxart: boxarts.filter(x => x.width == 150)
-											 									 .map(x => x.url)
-											 									 .reduce(((curr, item) => item + curr), '')
-												})
-											)
-									 	})
-									 .concatAll()
+const transformMovies = (movieLists) => {
+	return movieLists
+				.map(item => item.videos)
+				.map(videos => {
+					return videos.map(({title, id, boxarts}) => ({
+						title,
+						id,
+						boxart: (() => {
+														if(!boxarts) return
+
+														return boxarts.filter(x => x.width == 150)
+											 										.map(x => x.url)
+											 										.reduce(((curr, item) => item + curr), '')
+													})()
+						})
+					)
+				})
+				.concatAll()
 
 }
 
-const getVideos2 = (movieLists) => {
-	return movieLists.map(item => item.videos)
-									 .map(video => getDetails(video))
-									 .concatAll()
-}
-
-const getDetails = (videos) => {
-	return videos.map(({title, id, boxarts}) => ({
-			title,
-			id,
-			boxart: getBoxart(boxarts)
-		})
-	)
-}
-
-const getBoxart = (boxarts) => {
-	if(!boxarts) return
-
-	return boxarts.filter(x => x.width == 150)
-											 .map(x => x.url)
-											 .reduce(((curr, item) => item + curr), '')
-}
-
-module.exports = {
-	getVideos,
-	getDetails,
-	getBoxart
-}
+module.exports = transformMovies
 
--- app.spec.js
+++ app.spec.js
@@ -1,19 +1,19 @@
 const assert = require('assert')
-const app = require('./app')
+const transformMovies = require('./app')
 const data = require('./data')
 
 describe('app', () => {
-	it('gets videos', () => {
+	it('transforms movies', () => {
 		const expected = [
      {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
      {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
      {"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
      {"id": 675465,"title": "Fracture","boxart":"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
    	];
-		assert.deepEqual(app.getVideos(data), expected)
+		assert.deepEqual(transformMovies(data), expected)
 	})
 
-	it('gets videos with undefined boxart', () => {
+	it('gets movies with undefined boxart', () => {
 		const data = [
 			{
 				videos: [ { 'id': 70111470, 'title': 'title 1', 'other': 'meh'} ]
@@ -27,38 +27,6 @@ describe('app', () => {
 			{ 'id': 70111470, 'title': 'title 1', 'boxart': undefined },
 			{ 'id': 65432445, 'title': 'title 2', 'boxart': undefined }
 		]
-		assert.deepEqual(app.getVideos(data), expected)
-	})
-
-	it('gets id, title and boxart', () => {
-		const data = [
-			{ 'id': 70111470, 'title': 'title 1', 'other': 'meh',
-				'boxarts': [
-            { width: 200, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg' },
-            { width: 150, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg' }
-          ]
-      },
-			{ 'id': 65432445, 'title': 'title 2', 'other': 'meh',
-				 'boxarts': [
-          { width: 150, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/DieHard150.jpg' },
-          { width: 200, height: 200, url: 'http://cdn-0.nflximg.com/images/2891/DieHard200.jpg' }
-         ]
-      }
-		]
-
-		const expected = [
-			{ 'id': 70111470, 'title': 'title 1', 'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg'},
-			{ 'id': 65432445, 'title': 'title 2', 'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard150.jpg'}
-		]
-		assert.deepEqual(app.getDetails(data), expected)
-	})
-
-	it('gets boxart', () => {
-		const data = [
-      { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
-      { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
-		]
-		const expected = 'http://cdn-0.nflximg.com/images/2891/DieHard150.jpg'
-		assert.deepEqual(app.getBoxart(data), expected)
+		assert.deepEqual(transformMovies(data), expected)
 	})
 })
